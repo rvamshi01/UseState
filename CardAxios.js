766c8d1c0b1cc80e5f9e3ffc341fc5acde5ef8f7
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-18 12:54:49 +0530

    Update CardAxios.js

--- CardAxios.js
+++ CardAxios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function CardAxios({handleClick}) {
